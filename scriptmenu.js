5dd9acf79d24960746c9526c19e0d45ffaa79b39
imolena1984 <imolena@outlook.com.br>
2024-10-29 12:03:12 -0300

    Update scriptmenu.js
    REFORMULACAO MENU

--- scriptmenu.js
+++ scriptmenu.js
@@ -1,4 +1,11 @@
+// Seleciona o botão e o menu
+const hamburgerButton = document.querySelector('.hamburger');
+const menu = document.querySelector('.menu');
+
+// Função para alternar a visibilidade do menu
 function toggleMenu() {
-    const menu = document.querySelector('.menu');
-    menu.classList.toggle('show');
+    menu.classList.toggle('active');
 }
+
+// Adiciona um evento de clique ao botão hambúrguer
+hamburgerButton.addEventListener('click', toggleMenu);
